4f57c0c36daaf398477d5bcec1fe510ce8b650d6
weijie <807388755@qq.com>
2019-01-07 00:37:28 +0800

    first Commit：完成mybatis自动生成DO,Mapper,xml文件

---
+++ com-hrshop-android/src/main/java/com/project/huaruan/comhrshopandroid/ComHrshopAndroidApplication.java
@@ -0,0 +1,14 @@
+package com.project.huaruan.comhrshopandroid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComHrshopAndroidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComHrshopAndroidApplication.class, args);
+    }
+
+}
+
---
+++ com-hrshop-operate/src/main/java/com/project/huaruan/hrshop/operate/ComHrshopOperateApplication.java
@@ -0,0 +1,14 @@
+package com.project.huaruan.hrshop.operate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComHrshopOperateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComHrshopOperateApplication.class, args);
+    }
+
+}
+
